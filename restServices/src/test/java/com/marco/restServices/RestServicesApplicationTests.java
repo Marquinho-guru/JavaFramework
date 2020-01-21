91592a690f299a3d0dcdd7601b923b5f3007ebc1
Marquinho-guru <mzamudio23@hotmail.com>
2020-01-20 19:57:27 -0600

    First Commit
    Primer Commit

---
+++ restServices/src/test/java/com/marco/restServices/RestServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco.restServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
